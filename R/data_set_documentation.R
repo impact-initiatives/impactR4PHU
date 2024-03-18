51d6b4d2b8e81ffd400a3018c824523c75c4cb25
AbrahamAz <abraham.azar30@outlook.com>
2024-03-18 14:31:31 +0100

    minor fixes

---
+++ R/data_set_documentation.R
@@ -0,0 +1,18 @@
+############################### data documentation #######################
+
+
+#' Survey data
+#'
+#' These data sets include HH data used for the readme
+#'
+#'
+#' @format NULL
+#' @examples
+#' impactR4PHU_data_template
+
+
+#' @title MSNA template dataset (example)
+#' @name impactR4PHU_data_template
+#' @rdname impactR4PHU_data_template
+#' @format NULL
+"impactR4PHU_data_template"
