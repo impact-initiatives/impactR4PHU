ecbdb6a93761ef86931d992cc0526d28514cee1c
AbrahamAz <abraham.azar30@outlook.com>
2024-04-09 15:57:25 +0200

    some changes

--- inst/rmarkdown/templates/fsl-quality-report/skeleton/run_skeleton.R
+++ inst/rmarkdown/templates/fsl-quality-report/skeleton/run_skeleton.R
@@ -1,9 +1,15 @@
 rm(list = ls())
+chooseCRANmirror(ind = 1)
+utils::install.packages("rmarkdown")
+utils::install.packages("htmltools", )
+options(renv.consent = TRUE)
 strings <- c(
   out_date = stringr::str_sub(stringr::str_remove_all(Sys.Date(), '-'), 3)
 )
-Sys.setenv(RSTUDIO_PANDOC = "C:/Program Files/RStudio/resources/app/bin/quarto/bin/tools")
-file <- list.files("", ".Rmd", full.names = T)
+file <- list.files(getwd(), ".Rmd", full.names = T)
+if(!dir.exists(paste0(getwd(),"/output"))){
+  dir.create(paste0(getwd(),"/output"))
+}
 rmarkdown::render(file,
                   output_file = paste0("output/", "FSL_Quality_Check_and_Plausibility_", strings['out_date'],".html"))
 cat("\n> Quality Check completed! You can check your output folder.")
