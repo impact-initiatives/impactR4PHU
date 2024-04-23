2a75902f621eee985954b7698fdea6ccf5ebd9ff
AbrahamAz <abraham.azar30@outlook.com>
2024-04-23 14:07:01 +0200

    add plots for age distribution

---
+++ R/plot_age_distribution.R
@@ -0,0 +1,106 @@
+#' Plot Age Distribution
+#'
+#' @param .dataset HH_roster Data with Age and Sex of individuals
+#' @param by_group Disaggregated variable
+#' @param year_or_month Month or Year Plot. "year" for Year, "month" for Month.
+#' By deault it will calculate year.
+#' @param age_years the variable name in the Data indicating the year age of the individual
+#' By default: "age_years"
+#' @param age_months the variable name in the Data indicating the month age of the individual
+#' By default: "age_months"
+#' @param min_age Minimum Age cutoff. By default 0 for year and month.
+#' @param max_age Maximum Age cutoff. By default 5 for year and 59 for month.
+#' @param breaks Breaks of the Bins. By default 1 for year and 12 for month.
+#' @param file_path File path for saving the plot in a directory
+#' @param wdth width of the plot. By default: 5
+#' @param hght height of the plot. By default: 5
+#' @param title_name Title of the plot
+#'
+#' @return an age per year or month distribution plot
+#' @export
+#'
+#' @examples
+#' \dontrun{
+#'   plot_age_distribution(hh_roster)
+#' }
+
+
+plot_age_distribution <- function (.dataset,
+                                   by_group = NULL,
+                                   year_or_month = NULL,
+                                   age_years = "age_years",
+                                   age_months = "age_months",
+                                   min_age = NULL,
+                                   max_age = NULL,
+                                   breaks = NULL,
+                                   file_path = NULL,
+                                   wdth = 5,
+                                   hght = 5,
+                                   title_name = NULL) {
+  if(is.null(year_or_month) | year_or_month == "year"){
+    if (is.null(min_age)) {
+      min_age <- 0
+      print("No minimum age specified. Defaulting to 0 years.")
+    }
+    if (is.null(max_age)) {
+      max_age <- 5
+      print("No maximum age specified. Defaulting to 5 years.")
+    }
+    if (is.null(breaks)) {
+      breaks <- 1
+    }
+    .dataset <- .dataset %>% dplyr::filter(!!rlang::sym(age_years) >= min_age &
+                                 !!rlang::sym(age_years) <= max_age)
+    if (is.null(by_group)) {
+      g <- ggplot2::ggplot(data = .dataset, ggplot2::aes(x = get(age_years))) +
+        ggplot2::geom_histogram(binwidth = breaks) + ggplot2::scale_x_continuous(minor_breaks = seq(min_age,
+                                                                                                    max_age, by = 1), breaks = seq(min_age, max_age,
+                                                                                                                                  by = breaks), limits = c(min_age, max_age))
+    }
+    else {
+      g <- ggplot2::ggplot(data = .dataset, ggplot2::aes(x = get(age_years))) +
+        ggplot2::geom_histogram(binwidth = breaks) + ggplot2::scale_x_continuous(minor_breaks = seq(min_age,
+                                                                                                    max_age, by = 1), breaks = seq(min_age, max_age,
+                                                                                                                                   by = breaks), limits = c(min_age, max_age)) + ggplot2::facet_wrap(~get(by_group),
+                                                                                                                                                                                                     ncol = 1)
+    }
+  }
+
+  if(year_or_month == "month"){
+    if (is.null(min_age)) {
+      min_age <- 0
+      print("No minimum age specified. Defaulting to 0 years.")
+    }
+    if (is.null(max_age)) {
+      max_age <- 59
+      print("No maximum age specified. Defaulting to 5 years.")
+    }
+    if (is.null(breaks)) {
+      breaks <- 12
+    }
+
+    .dataset <- .dataset %>% dplyr::filter(!!rlang::sym(age_months) >= min_age &
+                                             !!rlang::sym(age_months) <= max_age)
+    if (is.null(by_group)) {
+      g <- ggplot2::ggplot(data = .dataset, ggplot2::aes(x = get(age_months))) +
+        ggplot2::geom_histogram(binwidth = breaks) + ggplot2::scale_x_continuous(minor_breaks = seq(min_age,
+                                                                                                    max_age, by = 1), breaks = seq(min_age, max_age,
+                                                                                                                                   by = breaks), limits = c(min_age, max_age))
+    }
+    else {
+      g <- ggplot2::ggplot(data = .dataset, ggplot2::aes(x = get(age_months))) +
+        ggplot2::geom_histogram(binwidth = breaks) + ggplot2::scale_x_continuous(minor_breaks = seq(min_age,
+                                                                                                    max_age, by = 1), breaks = seq(min_age, max_age,
+                                                                                                                                   by = breaks), limits = c(min_age, max_age)) + ggplot2::facet_wrap(~get(by_group),
+                                                                                                                                                                                                     ncol = 1)
+    }
+  }
+  if (!is.null(title_name)) {
+    g <- g + ggplot2::ggtitle(title_name)
+  }
+  if (!is.null(file_path)) {
+    ggplot2::ggsave(filename = file_path, width = wdth,
+                    height = hght)
+  }
+  return(g)
+}
---
+++ R/plot_age_pyramid.R
@@ -0,0 +1,63 @@
+#' Plot Age Pyramid
+#'
+#' @param .dataset HH_roster Data with Age and Sex of individuals
+#' @param sex_column the variable name in the Data indicating the sex of the individual
+#' By default: "sex"
+#' @param age_years the variable name in the Data indicating the age of the individual
+#' By default: "age_years"
+#' @param age_grouping If True, user using age_grouping in data with variable name
+#' "age_group". If default False, age_years will be grouped as followed.
+#' {0-4/5-9/10-14/15-19/20-24/25-29/30-34/35-39/40-44/
+#' 45-49/50-54/55-59/60-64/65-69/70-74/75-79/80-84/85+}
+#' @param file_path File path for saving the plot in a directory
+#' @param wdth width of the plot. By default: 5
+#' @param hght height of the plot. By default: 5
+#' @param title_name Title of the plot
+#'
+#' @return An Age Pyramid plot
+#' @export
+#'
+#' @examples
+#' \dontrun{
+#'   plot_age_pyramid(hh_roster)
+#' }
+plot_age_pyramid <- function (.dataset,
+                              sex_column = "sex",
+                              age_years = "age_years",
+                              age_grouping = FALSE,
+                              file_path = NULL,
+                              wdth = 5,
+                              hght = 5,
+                              title_name = NULL)
+{
+  print("Please note, the sex variable must be coded numerically by 1s (male) and 2s (female).")
+  if (age_grouping) {
+    .dataset <- .dataset %>%
+      dplyr::mutate(age_group = cut(as.numeric(!!rlang::sym(age_years)),
+                                    breaks = c(-1,4,9,14,19,24,29,34,39,44,49,54,59,64,69,74,79,84, Inf),
+                                    labels = c("0-4", "5-9", "10-14", "15-19",
+                                               "20-24", "25-29", "30-34", "35-39","40-44", "45-49", "50-54", "55-59",
+                                               "60-64", "65-69", "70-74", "75-79", "80-84", "85+")))
+
+  }
+  .dataset <- .dataset %>%
+    dplyr::arrange(!!rlang::sym(sex_column)) %>%
+    dplyr::rename(sex = sex_column)
+
+  g <- apyramid::age_pyramid(data = .dataset, age_group = "age_group",
+                               proportional = TRUE) +
+    ggplot2::ylab(paste0("Proportion of Population ")) +
+    ggplot2::scale_fill_manual(name = "Sex", labels = c("Male",
+                                                        "Female"),
+                               values = c("#08bcc4", "#ff746c"))
+  if (!is.null(title_name)) {
+    g <- g +
+      ggplot2::ggtitle(title_name)
+  }
+  if (!is.null(file_path)) {
+    ggplot2::ggsave(filename = file_path,
+                    width = wdth,
+                    height = hght)
+  }
+  return(g)
+}
