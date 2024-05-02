dbf695f915ef7e04ae4487c6e4d25ee6bd564e26
AbrahamAz <abraham.azar30@outlook.com>
2024-05-02 18:23:38 +0200

    minor fix

--- tests/testthat/test-create_fsl_descriptive_analysis_template.R
+++ tests/testthat/test-create_fsl_descriptive_analysis_template.R
@@ -4,12 +4,12 @@ testthat::test_that("Test basic functionality", {
   temp_dir <- tempdir()
   test_path <- file.path(temp_dir, "test_fsl_folder")
 
-  create_fsl_analysis_template(test_path)
+  create_fsl_descriptive_analysis_template(test_path)
 
   testthat::expect_true(fs::dir_exists(test_path))
 })
 
 
 testthat::test_that("Test invalid input handling", {
-  testthat::expect_error(create_fsl_analysis_template(NULL))
+  testthat::expect_error(create_fsl_descriptive_analysis_template(NULL))
 })
