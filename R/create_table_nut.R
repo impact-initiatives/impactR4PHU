1d7ba4433a8389d2cf9eb24d0d0a477bff4d3b97
Iyed GHEDAMSI <iyed.ghedamsi@impact-initiatives.org>
2025-07-02 15:56:34 +0300

    :twisted_rightwards_arrows: (package) Merging core changes from 'develop_muac_plaus'
    Merging changes to core functions of the package, while excluding anything related to templates

---
+++ R/create_table_nut.R
@@ -0,0 +1,144 @@
+#' create_table_nut
+#'
+#' @param .flextable Base flextable object
+#'
+#' @returns A flextable object
+#' @export
+create_table_nut <- function(.flextable) {
+  for (n in 1:12) {
+    k <- as.numeric(n + 1)
+    if ((n %% 2) == 0) {
+      next
+    } else {
+      .flextable <- flextable::merge_at(.flextable, i = n:k, j = 1)
+    }
+  }
+  for (n in 1:12) {
+    k <- as.numeric(n + 1)
+    if ((n %% 2) == 0) {
+      next
+    } else {
+      .flextable <- flextable::merge_at(.flextable, i = n:k, j = 6)
+    }
+  }
+
+  .flextable <- .flextable %>%
+    flextable::add_header_row(
+      values = c("", "Values", ""),
+      colwidths = c(1, 4, 1),
+      top = TRUE
+    ) %>%
+    flextable::align(
+      j = 2:6,
+      align = "center",
+      part = "body"
+    ) %>%
+    flextable::align(
+      j = 2:6,
+      align = "center",
+      part = "header"
+    ) %>%
+    flextable::border_inner_h() %>%
+    flextable::border_inner_v() %>%
+    flextable::border_inner_h(part = "header") %>%
+    flextable::surround(
+      part = "header",
+      border.top = flextable::fp_border_default(
+        color = "black",
+        style = "solid",
+        width = 2
+      )
+    ) %>%
+    flextable::surround(
+      part = "header",
+      border.bottom = flextable::fp_border_default(
+        color = "black",
+        style = "solid",
+        width = 2
+      )
+    ) %>%
+    flextable::surround(
+      part = "header",
+      border.left = flextable::fp_border_default(
+        color = "black",
+        style = "solid",
+        width = 2
+      )
+    ) %>%
+    flextable::surround(
+      part = "header",
+      border.right = flextable::fp_border_default(
+        color = "black",
+        style = "solid",
+        width = 2
+      )
+    ) %>%
+    flextable::surround(
+      i = 1,
+      j = 1:6,
+      border.top = flextable::fp_border_default(
+        color = "black",
+        style = "solid",
+        width = 3
+      )
+    ) %>%
+    flextable::surround(
+      i = 12,
+      j = 1:6,
+      border.bottom = flextable::fp_border_default(
+        color = "black",
+        style = "solid",
+        width = 3
+      )
+    ) %>%
+    flextable::surround(
+      i = 1:12,
+      j = 1,
+      border.left = flextable::fp_border_default(
+        color = "black",
+        style = "solid",
+        width = 3
+      )
+    ) %>%
+    flextable::surround(
+      i = 1:12,
+      j = 6,
+      border.right = flextable::fp_border_default(
+        color = "black",
+        style = "solid",
+        width = 3
+      )
+    ) %>%
+    flextable::surround(
+      i = 14,
+      j = 1:6,
+      border.bottom = flextable::fp_border_default(
+        color = "black",
+        style = "solid",
+        width = 3
+      )
+    ) %>%
+    flextable::surround(
+      i = 13:14,
+      j = 1,
+      border.left = flextable::fp_border_default(
+        color = "black",
+        style = "solid",
+        width = 3
+      )
+    ) %>%
+    flextable::surround(
+      i = 13:14,
+      j = 6,
+      border.right = flextable::fp_border_default(
+        color = "black",
+        style = "solid",
+        width = 3
+      )
+    ) %>%
+    flextable::bold(j = 1) %>%
+    flextable::bold(j = "Score") %>%
+    flextable::bold(part = "header")
+
+  return(.flextable)
+}
